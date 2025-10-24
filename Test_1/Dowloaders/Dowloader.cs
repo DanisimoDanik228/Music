a83d874ec0bec9254d445d62b65c1187b65309ca
Werty <Werty@DESKTOP-LB170S7>
2025-10-25 00:28:21 +0300

    First work version without selenium

---
+++ Test_1/Dowloaders/Dowloader.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Components.Sections;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
+using System.IO.Compression;
+using System.Net;
+using Test_1.Models;
+
+namespace Test_1.Dowloaders
+{
+
+    public static class Dowloader
+    {
+        public static string CompresToZip(string folder)
+        {
+            var parentFolder = Directory.GetParent(folder).FullName;
+            var zipFile = Path.Combine(parentFolder, DateTime.Now.ToString("HH-mm-ss")+".zip");
+
+            ZipFile.CreateFromDirectory(folder, zipFile, CompressionLevel.Optimal, includeBaseDirectory: false);
+
+            return zipFile;
+        }
+
+        public static async Task DowloadFilesAsync(IEnumerable<InfoSong> urls,string destination)
+        {
+            const int maxConcurrent = 5;
+
+            var semaphore = new SemaphoreSlim(maxConcurrent);
+            var tasks = urls.Select(async (info) =>
+            {
+                await semaphore.WaitAsync();
+                try
+                {
+                    await DowloadFileAsync(info, destination);
+                }
+                finally
+                {
+                    semaphore.Release();
+                }
+            });
+
+            await Task.WhenAll(tasks);
+        }
+
+        public async static Task<string> DowloadFileAsync(InfoSong info, string destination)
+        {
+            using (var client = new HttpClient())
+            {
+                var filename = $"{info.artist} - {info.songName}";
+                var fullPath = Path.Combine(destination, filename) + ".mp3";
+
+                try
+                {
+                    // Скачивание файла
+                    byte[] fileBytes = await client.GetByteArrayAsync(info.songUrl);
+                    await File.WriteAllBytesAsync(fullPath, fileBytes);
+
+                    try
+                    {
+                        //FixTitleName(fullPath, info);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error in FixTitleName: " + ex.Message);
+                    }
+
+                    //dowloadedFiles.Push(fullPath);
+                    return fullPath;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error downloading file: " + ex.Message);
+                    throw;
+                }
+            }
+        }
+    }
+}
---
+++ Test_1/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Net.Mime;
+using Test_1.Dowloaders;
+
+namespace Test_1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDistributedMemoryCache();
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
+            var app = builder.Build();
+            app.UseSession();
+
+            app.MapGet("/", async (HttpContext context) => {
+                context.Response.Headers.ContentType = "text/html; charset=utf-8";
+
+                await context.Response.SendFileAsync("Html\\Index.html");
+            });
+
+            app.MapPost("/",async (HttpContext context) => {
+                
+                var form = await context.Request.ReadFormAsync();
+                var nameSong = form["nameSong"];
+
+
+                var songs = await DowloadMp3Party.GetInfoSong(nameSong);
+
+                string destinationFolder = @"C:\Users\Werty\Desktop\test\" + Guid.NewGuid().ToString();
+
+                Directory.CreateDirectory(destinationFolder);
+
+               await Dowloader.DowloadFilesAsync(songs, destinationFolder);
+
+                var zipFile = Dowloader.CompresToZip(destinationFolder);
+
+                string resultHtml = $@"
+                    <h3>Найденные ссылки:</h3>
+                    {string.Join("<br>", songs.Select(l => $"<a href='{l.dowloadLink}' target='_blank'>{(l.artist + " - " + l.songName)}</a>"))}
+                    <br><br><a href='/'>Назад</a>
+
+                    <form method='post' action='/dowload'>
+                        <button type='submit'>Скачать zip</button>
+                    </form>
+                ";
+
+                context.Session.SetString("ZipFile", zipFile);
+
+                context.Response.Headers.ContentType = "text/html; charset=utf-8";
+                await context.Response.WriteAsync(resultHtml);
+            });
+
+            app.MapPost("/dowload", async (HttpContext context) => {
+                var zipFile = context.Session.GetString("ZipFile");
+
+                if (zipFile == null || !File.Exists(zipFile))
+                {
+                    context.Response.StatusCode = 404;
+                    await context.Response.WriteAsync(@"
+                            <html>
+                                <body>
+                                    <h3>Файл не найден или сессия устарела</h3>
+                                    <a href='/'>Вернуться на главную</a>
+                                </body>
+                            </html>
+                        ");
+
+                    return;
+                }
+                else
+                {
+                    var fileName = Path.GetFileName(zipFile);
+
+                    context.Response.Headers.ContentType = "application/zip";
+                    context.Response.Headers.ContentDisposition = $"attachment; filename=\"{fileName}\"";
+
+                    await context.Response.SendFileAsync(zipFile);
+
+                    //File.Delete(zipFile);
+                    context.Session.Remove("ZipFile");
+                }
+            });
+
+            app.Run();
+        }
+    }
+}
